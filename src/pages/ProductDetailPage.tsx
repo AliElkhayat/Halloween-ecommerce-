6e8368325f78b2ddc69ba782e7007f9393f3c4f1
Sallyzayed8 <sallymdzayed@gmail.com>
2025-05-12 23:12:34 +0300

    adding new files
    OrderConfirmationPage
    PrivacyPage
    ProductDetailPage
    ProductsPage

---
+++ src/pages/ProductDetailPage.tsx
@@ -0,0 +1,12 @@
+
+import { ProductDetails } from "@/components/ProductDetailsWrapper";
+
+const ProductDetailPage = () => {
+  return (
+    <div className="min-h-screen">
+      <ProductDetails />
+    </div>
+  );
+};
+
+export default ProductDetailPage;
