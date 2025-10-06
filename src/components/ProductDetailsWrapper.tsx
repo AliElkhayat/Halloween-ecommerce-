44dd45a81eba7adf9fc213e29622845f6634b74d
AKI <alielkhayat31@gmail.com>
2025-10-06 12:49:16 +0300

    Update ProductDetailsWrapper.tsx

--- src/components/ProductDetailsWrapper.tsx
+++ src/components/ProductDetailsWrapper.tsx
@@ -14,9 +14,6 @@ const mockProduct = {
   price: 49.99,
   category: "Decorations",
   images: [
-    "https://images.unsplash.com/photo-1509557965875-ef766d4a8a3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    "https://images.unsplash.com/photo-1604014091631-37938aa8ed95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-  ],
   stock: 10,
   featured: true
 };
